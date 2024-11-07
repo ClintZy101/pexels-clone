fb85a0e93fe416d02dbca53ca570e51bf15212c9
clintzy <clielizek.joy@gmail.com>
2024-11-07 10:13:57 +0800

    some fixes in video modal

--- src/components/modals/VideoDownloadModal.jsx
+++ src/components/modals/VideoDownloadModal.jsx
@@ -8,7 +8,10 @@ const VidDownloadModal = ({setIsOpen}) => {
     return (
       <div className="w-screen h-[1000px] bg-black bg-opacity-80 absolute top-0 z-50">
         <div className="relative md:w-[500px] md:h-[300px] w-full  inline-block bg-white z-50 rounded-lg mx-auto mt-[150px] md:grid place-items-center gap-5 p-5">
-          <LinkToHomeButton />
+            <div className=" w-full flex justify-end">
+            <LinkToHomeButton />
+            </div>
+          
           <div className="grid gap-5 ">
             <h1 className="font-bold text-xl ">Message:</h1>
             <p className="text-black font-semibold inline ">{message}</p>
